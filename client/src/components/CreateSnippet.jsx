a065a7b3b159b303023522fc2d51c06c896f4d2d
Ajai-3 <ajaipjayan@gmail.com>
2025-05-13 09:19:03 +0530

    microsevice study

---
+++ client/src/components/CreateSnippet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateSnippet = () => {
+  return (
+    <div>CreateSnippet</div>
+  )
+}
+
+export default CreateSnippet
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import CreateSnippet from '../components/CreateSnippet'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <CreateSnippet />
+    </div>
+  )
+}
+
+export default Home
